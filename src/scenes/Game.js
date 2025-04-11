a1fd106fa26bb023a3dc692c76a2b58e23fe10cd
Ayton-Low <low_pin_zun_ayton@students.edu.sg>
2025-04-11 06:24:10 +0000

    Loaded all images and reset ball, leftPaddle and rightPaddle

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,12 +6,20 @@ const HEIGHT = 768;
 export class Game extends Scene {
     constructor() {
         super('Game');
+        this.ball = null;
+        this.leftPaddle = null;
+        this.rightPaddle = null;
     }
 
+    
     preload() {
+        this.load.image('background', 'assets/background.png');
+        this.load.image('ball', 'assets/ball.png');
+        this.load.image('paddle', 'assets/paddle.png');
     }
 
     create() {
+
     }
 
     update() {
