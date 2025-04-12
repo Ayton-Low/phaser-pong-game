2df9b576ce7fb2106ecc7343774f84122cdc6ab3
Ayton-Low <low_pin_zun_ayton@students.edu.sg>
2025-04-12 10:10:47 +0000

    Bar smaller

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -16,7 +16,7 @@ export class Game extends Scene {
 
     create() {
         this.ball = this.add.circle(WIDTH/2, HEIGHT/2, 100, "0x0010ff");
-        this.healthBar = this.add.image(WIDTH/2, HEIGHT/2 - 128, 'healthBar').setScale(5, 1);
+        this.healthBar = this.add.image(WIDTH/2, HEIGHT/2 - 128, 'healthBar').setScale(2, 1);
         this.health = this.add.rectangle(WIDTH/2, HEIGHT/2 - 128, 100, 8, "0x00ff00");
     }
 
